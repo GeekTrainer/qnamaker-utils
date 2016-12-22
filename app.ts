262be6ecf117135a38fc180271f0222712365579
Christopher Harrison <chrhar@microsoft.com>
2016-12-21 17:48:23 -0800

    Updated to support new structure

--- app.ts
+++ app.ts
@@ -2,20 +2,17 @@ require('dotenv').config();
 
 import * as restify from 'restify';
 import * as builder from 'botbuilder';
-import Formatter from './formatter';
+import QnADialog from './qnadialog';
 
 class App {
     run() {
-        const server = restify.createServer(process.env.PORT);
         const bot = new builder.UniversalBot(new builder.ChatConnector());
 
-        bot.dialog('/', async (session) => {
-            
-            Formatter.format(session, '');
-            session.endConversation();
-        });
+        bot.dialog('/', new QnADialog('1f9546cac0b44a0a9b9832139ddc30c2', '64ac1b5c620f4a4bafbec15192bf284a'));
 
-        server.listen((<builder.ChatConnector> bot.connector('*')).listen());
+        const server = restify.createServer();
+        server.post('/api/messages', (<builder.ChatConnector> bot.connector('*')).listen());
+        server.listen(process.env.PORT);
     }
 }
 
